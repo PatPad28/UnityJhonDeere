356bc16ea0e8b82c33e725f04590eb0c8d507d4f
IngeMoreno23 <a01742930@tec.mx>
2025-12-03 06:37:23 -0600

    Added prefabs to instantiate on FarmClient done

--- Assets/Scripts/FarmClient.cs
+++ Assets/Scripts/FarmClient.cs
@@ -1,8 +1,10 @@
-using UnityEngine;
 using NativeWebSocket;
-using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class FarmClient : MonoBehaviour
 {
@@ -22,10 +24,15 @@ public class FarmClient : MonoBehaviour
 
     public GameObject CropInstancerObject;
 
+    public GameObject grassPrefab;
+    public GameObject dirtPrefab;
+
+    public GameObject[] rockPrefabs;
+
     private float nextLogTime = 0f;
     public float logInterval = 5.0f;
 
-    private Dictionary<(int, int), GameObject> visualCropGrid = new Dictionary<(int, int), GameObject>();
+    private GameObject[,] rockObstables;
 
     public class AgentData
     {
@@ -59,10 +66,21 @@ public class FarmClient : MonoBehaviour
         public MetaData meta;
     }
 
+    public class Parcel
+    {
+        public int minRow;
+        public int maxRow;
+        public int minCol;
+        public int maxCol;
+    }
+
     private Dictionary<int, GameObject> visualAgents = new Dictionary<int, GameObject>();
     private GameObject[,] visualGrid;
     private int gridRows = 0;
     private int gridCols = 0;
+
+    private List<Parcel> parcels = null;
+    private GameObject[,] soilGrid;
 
     async void Start()
     {
@@ -119,8 +137,8 @@ public class FarmClient : MonoBehaviour
         // Conversión de Coordenadas:
         // Python: [Fila (Y), Columna (X)]
         // Unity:  X = Columna, Z = Fila
-        float targetX = data.pos[1] * cellSize;
-        float targetZ = data.pos[0] * cellSize;
+        float targetX = data.pos[0] * cellSize;
+        float targetZ = data.pos[1] * cellSize;
         Vector3 targetPos = new Vector3(targetX, 0, targetZ);
 
         if (visualAgents.ContainsKey(data.id))
@@ -160,6 +178,46 @@ public class FarmClient : MonoBehaviour
             visualGrid = new GameObject[rows, cols];
         }
 
+        if (parcels == null)
+        {
+            parcels = DetectParcels(gridData);
+            soilGrid = new GameObject[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    bool inside = IsInParcel(r, c);
+
+                    GameObject prefab = inside ? dirtPrefab : grassPrefab;
+                    Vector3 pos = new Vector3(c * cellSize, -0.1f, r * cellSize);
+
+                    soilGrid[r, c] = Instantiate(prefab, pos, Quaternion.identity);
+                    soilGrid[r, c].transform.localScale = new Vector3(cellSize, cellSize, cellSize);
+                }
+            }
+        }
+
+        if (rockObstables == null)
+        {
+            rockObstables = new GameObject[rows, cols];
+            for (int r = 0;r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    int type = gridData[r][c];
+
+                    if (type == 1)
+                    {
+                        var rng = new System.Random();
+                        var rock = rockPrefabs[rng.Next(rockPrefabs.Length)];
+
+                        rockObstables[r, c] = Instantiate(rock, new Vector3(c * cellSize, 0, r * cellSize), Quaternion.identity);
+                    }
+                }
+            }
+        }
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < cols; c++)
@@ -168,7 +226,7 @@ public class FarmClient : MonoBehaviour
 
                 GameObject currentObj = visualGrid[r, c];
 
-                if (type  == 0)
+                if (type  == 0 || type == 3)
                 {
                     if (currentObj != null)
                     {
@@ -183,13 +241,94 @@ public class FarmClient : MonoBehaviour
                         Vector3 pos = new Vector3(c * cellSize, 0, r * cellSize);
                         GameObject go = Instantiate(CropInstancerObject, Vector3.zero, Quaternion.identity);
                         CropBunchInstancer instancer = go.GetComponent<CropBunchInstancer>();
-                        instancer.Init(new Vector2(pos.x, pos.z), cellSize, cellSize);
+                        instancer.Init(new Vector2(pos.x, pos.z), cellSize*0.9f, cellSize*0.9f);
                         visualGrid[r, c] = go;
                     }
                 }
             }
         }
     }
+
+    private List<Parcel> DetectParcels(int[][] grid)
+    {
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+
+        List<Parcel> result = new List<Parcel>();
+        bool[,] visited = new bool[rows, cols];
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                if (!visited[r, c] && grid[r][c] == 11) // enum border = 11
+                {
+                    int minR = r;
+                    int maxR = r;
+                    int minC = c;
+                    int maxC = c;
+
+                    Queue<(int, int)> q = new Queue<(int, int)>();
+                    q.Enqueue((r, c));
+                    visited[r, c] = true;
+
+                    while (q.Count > 0)
+                    {
+                        var (cr, cc) = q.Dequeue();
+
+                        minR = Mathf.Min(minR, cr);
+                        maxR = Mathf.Max(maxR, cr);
+                        minC = Mathf.Min(minC, cc);
+                        maxC = Mathf.Max(maxC, cc);
+
+                        int[] dr = { 1, -1, 0, 0 };
+                        int[] dc = { 0, 0, 1, -1 };
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            int nr = cr + dr[i];
+                            int nc = cc + dc[i];
+
+                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols)
+                            {
+                                if (!visited[nr, nc] && grid[nr][nc] == 11) // enum border = 11
+                                {
+                                    visited[nr, nc] = true;
+                                    q.Enqueue((nr, nc));
+                                }
+                            }
+                        }
+                    }
+
+                    result.Add(new Parcel
+                    {
+                        minRow = minR,
+                        maxRow = maxR,
+                        minCol = minC,
+                        maxCol = maxC
+                    });
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private bool IsInParcel(int row, int col)
+    {
+        if (parcels == null) return false;
+
+        foreach (var p in parcels)
+        {
+            if (row > p.minRow && row < p.maxRow &&
+                col > p.minCol && col < p.maxCol)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     private async void OnApplicationQuit()
     {
